296d509f6d5e1f65c9d6514095c9e13afb098bc5
TGurke13 <gauerket@yahoo.com>
2024-04-11 19:07:30 -0400

    data into pd

---
+++ mmdata.py
@@ -0,0 +1,15 @@
+import pandas as pd
+# Import data from CSV files
+ratings = pd.read_csv('C:\\Users\\thoma\\OneDrive\\Desktop\\School-Stuff\\Final_Project\\FP1\\MM_CSV\\538 Ratings.csv')
+coach_results = pd.read_csv('C:\\Users\\thoma\\OneDrive\\Desktop\\School-Stuff\\Final_Project\\FP1\\MM_CSV\\Coach Results.csv')
+team_results = pd.read_csv('C:\\Users\\thoma\\OneDrive\\Desktop\\School-Stuff\\Final_Project\\FP1\\MM_CSV\\Team Results.csv')
+tournament_matchups = pd.read_csv('C:\\Users\\thoma\\OneDrive\\Desktop\\School-Stuff\\Final_Project\\FP1\\MM_CSV\\Tournament Matchups.csv')
+team_resume = pd.read_csv('C:\\Users\\thoma\\OneDrive\\Desktop\\School-Stuff\\Final_Project\\FP1\\MM_CSV\\Resumes.csv')
+conference_results = pd.read_csv('C:\\Users\\thoma\\OneDrive\\Desktop\\School-Stuff\\Final_Project\\FP1\\MM_CSV\\Conference Results.csv')
+conference_away_neutral = pd.read_csv('C:\\Users\\thoma\\OneDrive\\Desktop\\School-Stuff\\Final_Project\\FP1\\MM_CSV\\Conference Stats Away Neutral.csv')
+conference_away = pd.read_csv('C:\\Users\\thoma\\OneDrive\\Desktop\\School-Stuff\\Final_Project\\FP1\\MM_CSV\\Conference Stats Away.csv')
+conference_home = pd.read_csv('C:\\Users\\thoma\\OneDrive\\Desktop\\School-Stuff\\Final_Project\\FP1\\MM_CSV\\Conference Stats Home.csv')
+conference_neutral = pd.read_csv('C:\\Users\\thoma\\OneDrive\\Desktop\\School-Stuff\\Final_Project\\FP1\\MM_CSV\\Conference Stats Neutral.csv')
+conference_stats = pd.read_csv('C:\\Users\\thoma\\OneDrive\\Desktop\\School-Stuff\\Final_Project\\FP1\\MM_CSV\\Conference Stats.csv')
+num_of_upsets = pd.read_csv('C:\\Users\\thoma\\OneDrive\\Desktop\\School-Stuff\\Final_Project\\FP1\\MM_CSV\\Upset Count.csv')
+heat_check = pd.read_csv('C:\\Users\\thoma\\OneDrive\\Desktop\\School-Stuff\\Final_Project\\FP1\\MM_CSV\\Heat Check Tournament Index.csv')
